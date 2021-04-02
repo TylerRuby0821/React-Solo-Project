8fb96870c7e990f5e1c8f56b5338838d51a213f3
tyler <tylerruby0314@yahoo.com>
2021-04-02 11:24:58 -0500

    Restyled most of login/signup

--- frontend/src/Components/LoginFormPage/index.js
+++ frontend/src/Components/LoginFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './LoginForm.css';
 
 function LoginFormPage() {
@@ -36,33 +37,41 @@ function LoginFormPage() {
   }
 
   return (
-    <form className='login__form' onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email:
-        <input
-          className = 'login__email'
-          type="text"
-          value={credential}
-          onChange={(e) => setCredential(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <input
-          className = 'login__password'
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Log In</button>
-      <button className= "submit" onClick={handleDemo}>Log In as Demo</button>
-    </form>
+    <div className='login__container'>
+      <form className='login__form' onSubmit={handleSubmit}>
+        <div className= 'logo'>
+          <NavLink exact to='/'><img src='/assets/roughdraftWriteItDown.png' alt='img' /></NavLink>
+        </div>
+        <div>
+          <input
+            placeholder='Email'
+            className = 'login__email'
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <input
+            placeholder='Password'
+            className = 'login__password'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <button type="submit">Log In</button>
+        <button className= "submit" onClick={handleDemo}>Log In as Demo</button>
+        <p>
+          <NavLink to='/signup'> Don't have an account? Sign Up!</NavLink>
+        </p>
+      </form>
+    </div>
   );
 }
 
--- frontend/src/Components/SignupFormPage/index.js
+++ frontend/src/Components/SignupFormPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 import * as sessionActions from "../../store/session";
 import './SignupFormPage.css';
 
@@ -29,48 +30,53 @@ function SignupFormPage() {
   };
 
   return (
-    <form className='signup__form' onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className='signup__container'>
+      <form className='signup__form' onSubmit={handleSubmit}>
+        <div className= 'logo'>
+          <NavLink exact to='/'><img src='/assets/roughdraftWriteItDown.png' alt='img' /></NavLink>
+        </div>
+          <input
+            placeholder='Email'
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            placeholder="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+
+          <input
+            placeholder='Password'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <input
+            placeholder='Confirm Password'
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+
+        <ul>
+          {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+        </ul>
+        <button type="submit">Sign Up</button>
+        <p>
+          <NavLink to='/login'>Already have an account? Log in!</NavLink>
+        </p>
+      </form>
+
+    </div>
   );
 }
 
